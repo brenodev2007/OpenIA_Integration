a38c71b3e08ed7c83d18c8f0edfcc4a2ef0e51ba
Breno <breno.soriani@gmail.com>
2025-03-12 18:38:57 -0300

    Utilizando a API 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,11 @@ package org.example;
 
 import com.theokanning.openai.completion.CompletionRequest;
 import com.theokanning.openai.completion.chat.ChatCompletionRequest;
+import com.theokanning.openai.completion.chat.ChatMessage;
+import com.theokanning.openai.completion.chat.ChatMessageRole;
 import com.theokanning.openai.service.OpenAiService;
+
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,10 +14,14 @@ public class Main {
         var user = "Gere 5 produtos";
         var system = "Voce é um gerador de produtos de caminhão para a empresa mercedes";
 
-        //utilizando a API
+        //utilizando a API DA OpenAI para completar o texto
         var service = new OpenAiService("your_token");
         var completionRequest = ChatCompletionRequest.builder()
                 .model("gpt-4")
+                .messages(Arrays.asList(
+                        new ChatMessage(ChatMessageRole.USER.value(), user),
+                        new ChatMessage(ChatMessageRole.SYSTEM.value(), system)
+                ))
                 .build();
         service.createChatCompletion(completionRequest).getChoices().forEach(System.out::println);
     }
