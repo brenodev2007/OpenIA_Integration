edd79fd8b2744979f422b2651e60365e68939ece
Breno <breno.soriani@gmail.com>
2025-03-12 18:36:58 -0300

    Utilizando a API

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,20 @@
 package org.example;
 
+import com.theokanning.openai.completion.CompletionRequest;
+import com.theokanning.openai.completion.chat.ChatCompletionRequest;
+import com.theokanning.openai.service.OpenAiService;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        var user = "Gere 5 produtos";
+        var system = "Voce é um gerador de produtos de caminhão para a empresa mercedes";
+
+        //utilizando a API
+        var service = new OpenAiService("your_token");
+        var completionRequest = ChatCompletionRequest.builder()
+                .model("gpt-4")
+                .build();
+        service.createChatCompletion(completionRequest).getChoices().forEach(System.out::println);
     }
 }
